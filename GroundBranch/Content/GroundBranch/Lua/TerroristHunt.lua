4148ccd3a508b4153e78157fbf8b2d045b6af17e
CK-AT <noone@nowhere.at>
2022-06-26 16:55:47 +0200

    TerroristHunt updated (utilizes SpawnQueue and AmbushManager now)

--- GroundBranch/Content/GroundBranch/Lua/TerroristHunt.lua
+++ GroundBranch/Content/GroundBranch/Lua/TerroristHunt.lua
@@ -1,16 +1,25 @@
-local AdminTools = require('AdminTools')
+local MSpawnsPriority       = require('Spawns.Priority')
+local MSpawnsQueue          = require('Spawns.Queue')
+local AdminTools 			= require('AdminTools')
+local MSpawnsAmbushManager  = require('Spawns.AmbushManager')
 
 local terroristhunt = {
 	UseReadyRoom = true,
 	UseRounds = true,
 	StringTables = { "TerroristHunt" },
-	
-	MissionTypeDescription = "[Solo/Co-Op] Locate and eliminate a predetermined number of enemies in the area of operations.",
-	
+	MissionTypeDescription = "[Solo/Co-Op] Locate and eliminate a (somewhat) predetermined number of enemies in the area of operations.",
 	PlayerTeams = {
 		BluFor = {
 			TeamId = 1,
 			Loadout = "NoTeam",
+		},
+	},
+	AiTeams = {
+		OpFor = {
+			TeamId = 100,
+			Tag = 'OpFor',
+			CalculatedAiCount = 0,
+			Spawns = nil
 		},
 	},
 	Settings = {
@@ -38,120 +47,41 @@ local terroristhunt = {
 			Value = 10,
 			AdvancedSetting = true,
 		},
+		AIMaxConcurrentCount = {
+			Min = 1,
+			Max = 50,
+			Value = 50,
+			AdvancedSetting = false,
+		},
+		DisplayDebugMessages = {
+			Min = 0,
+			Max = 1,
+			Value = 0,
+			AdvancedSetting = true,
+		},
+		TriggerAreasRatio = {
+			Min = 0,
+			Max = 100,
+			Value = 0,
+			AdvancedSetting = false,
+		},
 	},
-	OpForTeamTag = "OpFor",
-	PriorityTags = { "AISpawn_1", "AISpawn_2", "AISpawn_3", "AISpawn_4", "AISpawn_5",
-		"AISpawn_6_10", "AISpawn_11_20", "AISpawn_21_30", "AISpawn_31_40", "AISpawn_41_50" },
-
-	SpawnPriorityGroupIDs = { "AISpawn_11_20", "AISpawn_31_40" },
-	-- these define the start of priority groups, e.g. group 1 = everything up to AISPawn_11_20 (i.e. from AISpawn_1 to AISpawn_6_10), group 2 = AISpawn_11_20 onwards, group 3 = AISpawn_31_40 onwards
-	-- everything in the first group is spawned as before. Everything is spawned with 100% certainty until the T count is reached
-	-- subsequent priority groups are capped, ensuring that some lower priority AI is spawned, and everything else is randomised as much as possible
-	-- so overall the must-spawn AI will spawn (priority group 1) and a random mix of more important and (a few) less important AI will spawn fairly randomly
-
-	SpawnPriorityGroups = {},
-	-- this stores the actual groups as separate tables of spawns indexed by priority group
-	
-	LastSpawnPriorityGroup = 0,
-	-- the last priority group in which spawns were found
-	
-	ProportionOfPriorityGroupToSpawn = 0.7,
-	-- after processing all group 1 spawns, a total of N spawns remain. Spawn 70% of those as group 2 , then 70% of the remaining number as group 3, ... (or 100% if no more groups exist) 
-	
-	TotalNumberOfSpawnsFound = 0,
-	-- simple total of spawns placed in all priority groups
-		
-	AlwaysUseEveryPriorityOneSpawn = false,
-	-- if true, priority one spawns will be used up entirely before considering lower priorities
-	-- if false, behaviour differs depending on T count and number of P1 spawns. At least N% of spawns will be not P1 spawns, preventing all P1 spawns being used if need be
-	MinimumProportionOfNonPriorityOneSpawns = 0.15,
-	-- in which case, always use this proportion of non P1 spawns (15% by default), rounded down
-		
-	PriorityGroupedSpawns = {},
-	-- used for old AI spawn method
-	
-	BumRushMode = false,
+	SpawnQueue = nil,
+	AmbushManager = nil,
 }
 
-
-function terroristhunt:DumbTableCopy(MyTable)
-	local ReturnTable = {}
-	
-	for Key, TableEntry in ipairs(MyTable) do
-		table.insert(ReturnTable, TableEntry)
-	end
-	
-	return ReturnTable
-end
-
-
 function terroristhunt:PreInit()
-	local AllSpawns = gameplaystatics.GetAllActorsOfClass('GroundBranch.GBAISpawnPoint')
-	local PriorityIndex = 1
-	
-	local TotalSpawns = 0
+	print('Pre initialization')
+	self.AiTeams.OpFor.Spawns = MSpawnsPriority:Create()
+	self.SpawnQueue = MSpawnsQueue:Create(self.Settings.AIMaxConcurrentCount.Value)
+	self.AmbushManager = MSpawnsAmbushManager:Create(self.SpawnQueue, self.AiTeams.OpFor.Tag)
 
-	local CurrentPriorityGroup = 1
-	local CurrentGroupTotal = 0
-	local CurrentPriorityGroupSpawns = {}
-	-- this needs to be outside the loop
-	
-	self.SpawnPriorityGroups = {}
-	
-	--gamemode.SetPlayerTeamRole(PlayerTeams.BluFor.TeamId, "Attackers")
-	-- only need to set this once
-
-	-- Orders spawns by priority while allowing spawns of the same priority to be randomised.
-	for i, PriorityTag in ipairs(self.PriorityTags) do
-		local bFoundTag = false
-
-		if CurrentPriorityGroup <= #self.SpawnPriorityGroupIDs then
-			if PriorityTag == self.SpawnPriorityGroupIDs[CurrentPriorityGroup] then
-				-- we found the priority tag corresponding to the start of the next priority group
-				self.SpawnPriorityGroups[CurrentPriorityGroup] = self:DumbTableCopy(CurrentPriorityGroupSpawns)
-				print("PreInit(): " .. CurrentGroupTotal .. " total spawns found for priority group " .. CurrentPriorityGroup )
-				CurrentPriorityGroup = CurrentPriorityGroup + 1
-				CurrentGroupTotal = 0
-				CurrentPriorityGroupSpawns = {}
-			end
-		end
-
-		
-		for j, SpawnPoint in ipairs(AllSpawns) do
-			if actor.HasTag(SpawnPoint, PriorityTag) then
-				bFoundTag = true
-				if self.PriorityGroupedSpawns[PriorityIndex] == nil then
-					self.PriorityGroupedSpawns[PriorityIndex] = {}
-				end
-				-- Ensures we can't spawn more AI then this map can handle.
-				TotalSpawns = TotalSpawns + 1 
-				table.insert(self.PriorityGroupedSpawns[PriorityIndex], SpawnPoint)
-				-- this is the table for the old method, which we may still want to use e.g. at low T counts
-
-				table.insert(CurrentPriorityGroupSpawns, SpawnPoint)
-				CurrentGroupTotal = CurrentGroupTotal + 1
-				-- also store in the table of spawnpoints for the new method
-			end
-		end
-
-		-- Ensures we don't create empty tables for unused priorities.
-		if bFoundTag then
-			PriorityIndex = PriorityIndex + 1
-			self.LastSpawnPriorityGroup = CurrentPriorityGroup
-		end
-	end
-	
-	self.SpawnPriorityGroups[CurrentPriorityGroup] = CurrentPriorityGroupSpawns
-	print("PreInit(): " .. CurrentGroupTotal .. " total spawns found for priority group " .. CurrentPriorityGroup )
-	self.TotalNumberOfSpawnsFound = TotalSpawns
-	
-	TotalSpawns = math.min(ai.GetMaxCount(), TotalSpawns)
+	TotalSpawns = math.min(ai.GetMaxCount(), self.AiTeams.OpFor.Spawns.Total)
 	self.Settings.OpForCount.Max = TotalSpawns
 	self.Settings.OpForCount.Value = math.min(self.Settings.OpForCount.Value, TotalSpawns)
 
 	self.Settings.ShowRemaining.Max = TotalSpawns
 	self.Settings.ShowRemaining.Value = math.min(self.Settings.ShowRemaining.Value, TotalSpawns)
-
 end
 
 function terroristhunt:PostInit()
@@ -203,10 +133,15 @@ function terroristhunt:CheckReadyDownTimer()
 end
 
 function terroristhunt:OnRoundStageSet(RoundStage)
+	print('Started round stage ' .. RoundStage)
+	timer.ClearAll()
+	self.SpawnQueue:Start()
 	if RoundStage == "WaitingForReady" then
-		ai.CleanUp(self.OpForTeamTag)
-		self.BumRushMode = false
+		ai.CleanUp(self.AiTeams.OpFor.Tag)
+		self.SpawnQueue:SetMaxConcurrentAICount(self.Settings.AIMaxConcurrentCount.Value)
+		self.SpawnQueue:Reset()
 	elseif RoundStage == "PreRoundWait" then
+		self.AmbushManager:ActivateRandomly(self.Settings.TriggerAreasRatio.Value)
 		self:SpawnOpFor()
 		gamemode.SetDefaultRoundStageTime("InProgress", self.Settings.RoundTime.Value)
 		-- need to update this as ops board setting may have changed - have to do this before RoundStage InProgress to be effective
@@ -217,121 +152,16 @@ function terroristhunt:OnRoundStageSet(RoundStage)
 end
 
 function terroristhunt:SpawnOpFor()
-	-- reorganised 8/9 September 2021 by MF to improve randomisation and use of all spawns while still respecting priorities
-	-- (it gets quite complicated)
-
-	local OrderedSpawns = {}
-
-	-------------------------
-	if "UseOldMethod" == "True" then
-		-- just park this here for now - old method, not used
-		for Key, Group in ipairs(self.PriorityGroupedSpawns) do
-			for i = #Group, 1, -1 do
-				local j = umath.random(i)
-				Group[i], Group[j] = Group[j], Group[i]
-				table.insert(OrderedSpawns, Group[i])
-			end
-		end
-	end
-	--------------------
-	
-	local RejectedSpawns = {}
-	local Group 
-	local AILeftToSpawn
-
-	for CurrentPriorityGroup = 1, self.LastSpawnPriorityGroup do
-						
-		AILeftToSpawn =  math.max( 0, self.Settings.OpForCount.Value - #OrderedSpawns )
-		-- this will be zero if the T count is already reached
-		
-		local CurrentAISpawnTarget 
-		-- number of spawns to try and add from this priority group
-		
-		-- determine how many spawns we're aiming for:
-		if AILeftToSpawn > 0 then
-			if CurrentPriorityGroup == 1 then
-				if self.AlwaysUseEveryPriorityOneSpawn then
-					CurrentAISpawnTarget = AILeftToSpawn
-				else
-					CurrentAISpawnTarget = math.ceil( AILeftToSpawn * (1 - self.MinimumProportionOfNonPriorityOneSpawns) )
-					-- leave a few slots spare for lower priorities (default 15%)
-					-- if the number of priority 1 spawns is lower than this number, then all priority 1 spawns will be used
-					-- (this only has an effect if there are lots of P1 spawns and not a big T count)
-				end
-				
-			elseif CurrentPriorityGroup == self.LastSpawnPriorityGroup then
-				CurrentAISpawnTarget = AILeftToSpawn
-				-- if this is the first group, or the last group, then try spawn all of the AI
-				
-			else
-				local CurrentNumberOfSpawns = #self.SpawnPriorityGroups[CurrentPriorityGroup]
-				local RemainingSpawnsInLowerPriorities = math.max( 0, self.TotalNumberOfSpawnsFound - CurrentNumberOfSpawns - #OrderedSpawns)
-				local CurrentProportionOfSpawnsLeft =  CurrentNumberOfSpawns / ( CurrentNumberOfSpawns + (RemainingSpawnsInLowerPriorities * self.ProportionOfPriorityGroupToSpawn) ) 
-				-- spawn a suitable number of spawns in dependence on the number of spawns in this group vs number of spawns remaining in lower groups, but fudge it to be bigger than the actual proportion
-				
-				CurrentAISpawnTarget = math.ceil(AILeftToSpawn * CurrentProportionOfSpawnsLeft)
-				
-				--print("SpawnOpFor(): found " .. CurrentNumberOfSpawns .. " spawns in current priority group " .. CurrentPriorityGroup .. ", vs " .. RemainingSpawnsInLowerPriorities .. " spawns in lower priorities.")
-				--print("SpawnOpFor(): using fudged proportion of " .. CurrentProportionOfSpawnsLeft)
-				
-				-- starting with 70%, so if 10 AI are left to spawn, we will attempt to spawn 7 of them from this group
-			end
-		else
-			CurrentAISpawnTarget = 0
-			-- no AI left to spawn so don't bother spawning any - just dump straight into RejectedSpawns{}
-		end
-
-
-		print("SpawnOpFor(): Picking max " .. CurrentAISpawnTarget .. " AI from priority group " .. CurrentPriorityGroup .. " with " .. AILeftToSpawn .. " AI left to spawn out of " .. self.Settings.OpForCount.Value .. " total.")
-
-		-- now transfer the appropriate number of spawns (randomly picked) to the target list (OrderedSpawns)
-		-- and dump the remainder in the RejectedSpawns table (to be added to the end of the target list once completed)
-		
-		Group = self.SpawnPriorityGroups[CurrentPriorityGroup]
-
-		if Group == nil then
-			print("SpawnOpFor(): Table entry for priority group " .. CurrentPriorityGroup.. " was unexpectedly nil")
-		else
-		
-			print("SpawnOpFor(): actually found " .. #Group .. " AI in group " .. CurrentPriorityGroup)
-		
-			if #Group > 0 then
-				for i = #Group, 1, -1 do
-					local j = umath.random(i)
-					Group[i], Group[j] = Group[j], Group[i]
-
-					if CurrentAISpawnTarget > 0 then
-						table.insert(OrderedSpawns, Group[i])
-						CurrentAISpawnTarget = CurrentAISpawnTarget - 1
-					else
-						table.insert(RejectedSpawns, Group[i])
-					end
-				end
-				-- ^ shuffle this group to randomise
-					
-			else
-				print("SpawnOpFor(): Priority group " .. CurrentPriorityGroup.. " was unexpectedly empty")
-			end
-			
-		end
-	end
-	
-	-- now add all the rejected spawns onto the list, in case extra spawns are needed
-	-- if we ran out of spawns in the above process, this will still provide a sensible selection of spawns
-	
-	--print("SpawnOpFor(): topping off list of spawns with " .. #RejectedSpawns .. " excess/rejected spawns")
-	
-	for i = 1, #RejectedSpawns do
-		table.insert(OrderedSpawns, RejectedSpawns[i])
-	end
-
-	ai.CreateOverDuration(4.0, self.Settings.OpForCount.Value, OrderedSpawns, self.OpForTeamTag)
+	self.AiTeams.OpFor.Spawns:SelectSpawnPoints()
+	self.AiTeams.OpFor.Spawns:EnqueueSpawning(self.SpawnQueue, 0.0, 0.4, self.Settings.OpForCount.Value, self.AiTeams.OpFor.Tag)
 end
 
 function terroristhunt:OnCharacterDied(Character, CharacterController, KillerController)
 	if gamemode.GetRoundStage() == "PreRoundWait" or gamemode.GetRoundStage() == "InProgress" then
 		if CharacterController ~= nil then
-			if actor.HasTag(CharacterController, self.OpForTeamTag) then
+			if actor.HasTag(CharacterController, self.AiTeams.OpFor.Tag) then
+				self.SpawnQueue:OnAIKilled()
+				AdminTools:ShowDebug(self.SpawnQueue:GetStateMessage())
 				timer.Set("CheckOpForCount", self, self.CheckOpForCountTimer, 1.0, false)
 			else
 				AdminTools:NotifyKIA(CharacterController)
@@ -350,17 +180,20 @@ function terroristhunt:OnCharacterDied(Character, CharacterController, KillerCon
 	end
 end
 
-function terroristhunt:CheckOpForCountTimer()
-	local OpForControllers = ai.GetControllers('GroundBranch.GBAIController', self.OpForTeamTag, 255, 255)
+function terroristhunt:OnMissionSettingChanged(Setting, NewValue)
+	AdminTools.ShowDebugGameMessages = self.Settings.DisplayDebugMessages.Value == 1
+	self.SpawnQueue:SetMaxConcurrentAICount(self.Settings.AIMaxConcurrentCount.Value)
+end
 
-	if #OpForControllers == 0 then
+function terroristhunt:CheckOpForCountTimer()
+	if self.SpawnQueue.AliveAICount == 0 then
 		timer.Clear("ShowRemaining")
 		gamemode.AddGameStat("Result=Team1")
 		gamemode.AddGameStat("Summary=OpForEliminated")
 		gamemode.AddGameStat("CompleteObjectives=EliminateOpFor")
 		gamemode.SetRoundStage("PostRoundWait")
-	elseif self.Settings.ShowRemaining.Value > 0 and #OpForControllers <= self.Settings.ShowRemaining.Value then
-		self.RemainingMessage = "RemainingOpFor" .. tostring(#OpForControllers)
+	elseif self.Settings.ShowRemaining.Value > 0 and self.SpawnQueue.AliveAICount <= self.Settings.ShowRemaining.Value then
+		self.RemainingMessage = "RemainingOpFor" .. tostring(self.SpawnQueue.AliveAICount)
 		timer.Set("ShowRemaining", self, self.ShowRemainingTimer, 10, false)
 	end
 end
