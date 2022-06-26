5b1aa59689d0bb335fba2cfae6cf1bcbf424d659
CK-AT <noone@nowhere.at>
2022-06-26 16:51:59 +0200

    Spawns.Priority instantiation fixed; Priority:EnqueueSpawning added

--- GroundBranch/Content/GroundBranch/Lua/Spawns/Priority.lua
+++ GroundBranch/Content/GroundBranch/Lua/Spawns/Priority.lua
@@ -11,13 +11,16 @@ local Priority = {
     Selected = {}
 }
 
+Priority.__index = Priority
+
 ---Creates new Priority spawns object.
 ---@return table Priority Newly created Priority spawns object.
 function Priority:Create()
-    local priority = {}
-    setmetatable(priority, self)
-    self.__index = self
+    local self = setmetatable({}, Priority)
 	self.Spawns = {}
+    self.Total = 0
+	self.Tags = Priority.Tags
+	self.Selected = {}
 	local priorityIndex = 1
 	for _, priorityTag in ipairs(self.Tags) do
 		local spawnsWithTag = gameplaystatics.GetAllActorsOfClassWithTag(
@@ -32,7 +35,7 @@ function Priority:Create()
 	end
 	print('Found ' .. self.Total .. ' spawns by priority')
 	print('Initialized PrioritySpawns ' .. tostring(priority))
-    return priority
+    return self
 end
 
 ---Shuffles priority grouped spawns. Ensures spawns of higher priority will be
@@ -60,6 +63,25 @@ function Priority:Spawn(duration, count, spawnTag)
 		self.Selected,
 		spawnTag
 	)
+end
+
+---Schedules AI spawning in the selected spawn points.
+---@param spawnQueue table The queue object to use.
+---@param delay number The time after which spawning shall start.
+---@param freezeTime number the time for which the ai should be frozen.
+---@param count integer The amount of the AI to spawn.
+---@param spawnTag string The tag that will be assigned to spawned AI.
+---@param preSpawnCallback function A function to call immediately before the first AI is spawned (optional).
+---@param preSpawnCallbackOwner table A owner object of preSpawnCallback (optional).
+---@param postSpawnCallback function A function to call after spawning is complete (optional).
+---@param postSpawnCallbackOwner table A owner object of postSpawnCallback (optional).
+---@param isBlocking boolean If set to true, the next queue item will only be processed after all AI have been spawned (optional, default = false).
+---@param prio number Higher prios will spawn before (optional, default = 255).
+function Priority:EnqueueSpawning(spawnQueue, delay, freezeTime, count, spawnTag, preSpawnCallback, preSpawnCallbackOwner, postSpawnCallback, postSpawnCallbackOwner, isBlocking, prio)
+    if count > #self.Selected then
+        count = #self.Selected
+    end
+	spawnQueue:Enqueue(delay, freezeTime, count, self.Selected, spawnTag, preSpawnCallback, preSpawnCallbackOwner, postSpawnCallback, postSpawnCallbackOwner, isBlocking, prio)
 end
 
 return Priority
