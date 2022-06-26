9c28f850c2dbaea8ac1fdf95cbfbbbba957bc615
CK-AT <noone@nowhere.at>
2022-06-26 14:28:45 +0200

    AmbushManager: 'Mandatory' tag implemented, activation behaviour across rounds fixed

--- GroundBranch/Content/GroundBranch/Lua/Spawns/AmbushManager.lua
+++ GroundBranch/Content/GroundBranch/Lua/Spawns/AmbushManager.lua
@@ -6,14 +6,15 @@ local AmbushManager = {
     SpawnQueue = nil,
     TeamTag = nil,
     Triggers = {},
-    TriggerKeys = {}
+    MandatoryTriggerKeys = {},
+    OptionalTriggerKeys = {}
 }
 
 AmbushManager.__index = AmbushManager
 
 ---Creates a new ambush manager object. At creation all relevant spawn points are
 ---gathered, default values are set.
----@return table AmbushManager Newly create AmbushManager object.
+---@return table AmbushManager Newly created AmbushManager object.
 function AmbushManager:Create(spawnQueue, teamTag)
     local self = setmetatable({}, AmbushManager)
     -- Setting attributes
@@ -29,41 +30,64 @@ function AmbushManager:Create(spawnQueue, teamTag)
                     Name = Name,
                     Tag = Tag,
                     Actor = Trigger,
-                    IsActive = false,
-                    IsTriggered = false,
+                    State = 'Inactive',
                     Spawns = gameplaystatics.GetAllActorsOfClassWithTag(
                         'GroundBranch.GBAISpawnPoint',
                         Tag
                     )
                 }
-                table.insert(self.TriggerKeys, Name)
-                print('Ambush trigger ' .. Tag .. ' found, has ' .. #self.Triggers[Name].Spawns .. ' associated AI spawns')
+                if actor.HasTag(Trigger, 'Mandatory') then
+                    print('Mandatory ambush trigger ' .. Tag .. ' found, has ' .. #self.Triggers[Name].Spawns .. ' associated AI spawns')
+                    table.insert(self.MandatoryTriggerKeys, Name)
+                else
+                    print('Optional ambush trigger ' .. Tag .. ' found, has ' .. #self.Triggers[Name].Spawns .. ' associated AI spawns')
+                    table.insert(self.OptionalTriggerKeys, Name)
+                end
                 break
             end
         end
     end
+    print('Found a total of ' .. #self.MandatoryTriggerKeys .. ' mandatory and ' .. #self.OptionalTriggerKeys .. ' optional ambush triggers.')
     return self
 end
 
 function AmbushManager:ActivateTrigger(Trigger)
-    print('Activating ambush trigger ' .. Trigger.Name .. ' ...')
-    Trigger.IsActive = true
-    Trigger.IsTriggered = false
+    print('Activating ambush trigger ' .. Trigger.Name .. '...')
+    Trigger.State = 'Active'
     actor.SetActive(Trigger.Actor, true)
 end
 
+function AmbushManager:DeactivateTrigger(Trigger)
+    print('Deactivating ambush trigger ' .. Trigger.Name .. '...')
+    Trigger.State = 'Inactive'
+    actor.SetActive(Trigger.Actor, false)
+end
+
 function AmbushManager:ActivateAll()
-    print('Activating all ambush triggers ...')
+    print('Activating all ambush triggers...')
     for _, Trigger in pairs(self.Triggers) do
         self:ActivateTrigger(Trigger)
     end
 end
 
+function AmbushManager:DeactivateAll()
+    print('Deactivating all ambush triggers...')
+    for _, Trigger in pairs(self.Triggers) do
+        self:DeactivateTrigger(Trigger)
+    end
+end
+
 function AmbushManager:ActivateRandomly(percentage)
+    self:DeactivateAll()
+    print('Activating all mandatory ambush triggers (' .. #self.MandatoryTriggerKeys .. ')...')
+    for i, Key in ipairs(self.MandatoryTriggerKeys) do
+        local Trigger = self.Triggers[Key]
+        self:ActivateTrigger(Trigger)
+    end
     percentage = percentage or 100
-    local count = math.min(math.floor((#self.TriggerKeys * percentage) / 100), #self.TriggerKeys)
-    print('Activating ' .. percentage .. '% of ambush triggers (' .. count .. ') ...')
-    for i, Key in ipairs(Tables.ShuffleTable(self.TriggerKeys)) do
+    local count = math.min(math.floor((#self.OptionalTriggerKeys * percentage) / 100), #self.OptionalTriggerKeys)
+    print('Activating ' .. percentage .. '% of optional ambush triggers (' .. count .. ')...')
+    for i, Key in ipairs(Tables.ShuffleTable(self.OptionalTriggerKeys)) do
         if i > count then
             break
         end
@@ -75,8 +99,9 @@ end
 function AmbushManager:OnGameTriggerBeginOverlap(GameTrigger, Player)
     local Trigger = self.Triggers[actor.GetName(GameTrigger)]
     if Trigger ~= nil then
-        if not Trigger.IsTriggered then
-            Trigger.IsTriggered = true
+        if Trigger.State == 'Active' then
+            Trigger.State = 'Triggered'
+            actor.SetActive(Trigger.Actor, false)
             local tiAmbush = math.random(0, 150) * 0.1
             local sizeAmbush = math.min(math.random(0, 10), #Trigger.Spawns)
             AdminTools:ShowDebug("Ambush " .. Trigger.Tag .. " triggered, spawning " .. sizeAmbush .. " AI in " .. tiAmbush .. "s")
