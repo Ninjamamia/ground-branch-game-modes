af35603e6a19b689c3d847b2d340440892b8543b
CK-AT <noone@nowhere.at>
2022-10-06 19:19:47 +0200

    TriggerLaptop: added timeout and made all parameters configurable via tags

--- GroundBranch/Content/GroundBranch/Lua/TriggerLaptop.lua
+++ GroundBranch/Content/GroundBranch/Lua/TriggerLaptop.lua
@@ -1,41 +1,86 @@
 local laptop = {
 	CurrentTime = 0,
+	OnSearch = "Are you in for surprises?",
+	OnSuccess = "There you go...",
+	SearchTime = 10.0,
+	Name = "TriggerLaptop",
+	Timers = {
+		Timeout = {
+			Name = 'Timeout',
+			TimeStep = 1.0,
+		}
+	}
 }
 
 function laptop:ServerUseTimer(User, DeltaTime)
 	self.CurrentTime = self.CurrentTime + DeltaTime
-	local SearchTime = 10.0
-	if gamemode.script.Settings.SearchTime ~= nil then
-		SearchTime = gamemode.script.Settings.SearchTime.Value
-	end
 	self.CurrentTime = math.max(self.CurrentTime, 0)
-	self.CurrentTime = math.min(self.CurrentTime, SearchTime)
+	self.CurrentTime = math.min(self.CurrentTime, self.SearchTime)
 
 	local Result = {}
-	Result.Message = "Are you in for surprises?"
+	Result.Message = self.OnSearch
 	Result.Equip = false
-	Result.Percentage = self.CurrentTime / SearchTime
+	Result.Percentage = self.CurrentTime / self.SearchTime
 	if Result.Percentage == 1.0 then
-		Result.Message = "There you go..."
+		timer.Clear(self.Timers.Timeout.Name, self)
+		Result.Message = self.OnSuccess
 		gamemode.script:OnLaptopTriggered(self.Object)
+	else
+		timer.Set(
+			self.Timers.Timeout.Name,
+			self,
+			self.CheckTimeoutTimer,
+			self.Timers.Timeout.TimeStep,
+			false
+		)
 	end
 	return Result
 end
 
-function laptop:OnReset()
+function laptop:CheckTimeoutTimer()
+	print(self.Name .. ": Timeout")
 	self.CurrentTime = 0
 end
 
+function laptop:OnReset()
+	self.Name = actor.GetName(self.Object)
+	print(self.Name .. ": Reset")
+	self.CurrentTime = 0
+	self.Timers.Timeout.Name = "Timeout_" .. actor.GetName(self.Object)
+	if gamemode.script.Settings.SearchTime ~= nil then
+		SearchTime = gamemode.script.Settings.SearchTime.Value
+	end
+	for _, Tag in ipairs(actor.GetTags(self.Object)) do
+		local key
+		local value
+		_, _, key, value = string.find(Tag, "(%a+)%s*=%s*(.*)")
+		if key ~= nil then
+			if key == "OnSearch" then
+				self.OnSearch = value
+			elseif key == "OnSuccess" then
+				self.OnSuccess = value
+			elseif key == "SearchTime" then
+				self.SearchTime = tonumber(value)
+			elseif key == "Timeout" then
+				self.Timers.Timeout.TimeStep = tonumber(value)
+			end
+		end
+	end
+end
+
 function laptop:LaptopPickedUp()
+	print(self.Name .. ": Picked up")
 	gamemode.script:OnLaptopPickedUp(self.Object)
 end
 
 function laptop:LaptopPlaced(NewLaptop)
+	print(self.Name .. ": Placed")
 	gamemode.script:OnLaptopPlaced(NewLaptop)
 end
 
 
 function laptop:CarriedLaptopDestroyed()
+	print(self.Name .. ": Destroyed")
 	gamemode.script:OnLaptopDestroyed(self.Object)
 end
 
